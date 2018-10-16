08d7c3693f02ada9a7108113ea4bd99b04a71052
Jerry Castro <leom551@hotmail.com>
2018-10-16 02:56:55 -0300

    Criação de Menu e adição da folha de estilos Header.css

--- view/Home.php
+++ view/Home.php
@@ -1,112 +1,36 @@
 <?php
 session_start();
 
-
-echo "usuario logado" . $_SESSION['nome'];
-
-
 require_once("../view/header.php");
+
 ?>
+<style type="text/css">
 
+.table1 {	
+	width: 30vw; 
+	border: solid 1px #ddd; 
+	font-size: 6vh; 
+	padding: 1vh; 
+	margin-top: 10px; 
+	text-align: center;
+}
+
+</style>
 <body>
-
-<div id="page-wrapper">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h1 class="page-header">Home</h1>
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
-            <!-- /.row -->
-            <div class="row">
-                <div class="col-lg-4">
-                    <div class="panel panel-primary">
-                        <div class="panel-heading">
-                            Mensagens
-                        </div>
-                        <!-- /.panel-heading -->
-                        <div class="panel-body">
-                            <div class="table-responsive">
-                                <table class="table">
-                                    <thead>
-                                        <tr>
-                                            <th>#</th>
-                                            <th>Mensagem</th>
-                                            <th>Assunto</th>
-                                            <th>Nome</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        
-                                    </tbody>
-                                </table>
-                            </div>
-                            <!-- /.table-responsive -->
-                        </div>
-                        <!-- /.panel-body -->
-                    </div>
-                    <!-- /.panel -->
-                </div>
-                <!-- /.col-lg-6 -->
-                <div class="col-lg-5">
-                    <div class="panel panel-primary">
-                        <div class="panel-heading">
-                            Tarefas
-                        </div>
-                        <!-- /.panel-heading -->
-                        <div class="panel-body">
-                            <div class="table-responsive">
-                                <table class="table">
-                                    <thead>
-                                        <tr>
-                                            <th>#</th>
-                                            <th>Tarefa</th>
-                                            <th>Nome</th>
-                                            <th>Prazo</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                       
-                                    </tbody>
-                                </table>
-                            </div>
-                            <!-- /.table-responsive -->
-                        </div>
-                        <!-- /.panel-body -->
-                    </div>
-                    <!-- /.panel -->
-                </div>
-                <!-- /.col-lg-6 -->
-                <div class="col-lg-3">
-                    <div class="panel panel-primary">
-                        <div class="panel-heading">
-                            Video Aulas
-                        </div>
-                        <!-- /.panel-heading -->
-                        <div class="panel-body">
-                            <div class="table-responsive">
-                                <table class="table">
-                                    <thead>
-                                        <tr>
-                                            <th>#</th>
-                                            <th>Video Aulas</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                       
-                                    </tbody>
-                                </table>
-                            </div>
-                            <!-- /.table-responsive -->
-                        </div>
-                        <!-- /.panel-body -->
-                    </div>
-                    <!-- /.panel -->
-                </div>
-                <!-- /.col-lg-6 -->
-            </div>
-            <!-- /.row -->
-            
+	<div style = "width: 100vw; padding: 1vh 5vw 0vw 5vw">
+		<table> 
+			<th class="table1">
+				Mensagens
+			</th>
+			<th class="table1">
+				Tarefas
+			</th>
+			<th class="table1"	>
+				Treinamentos
+			</th>
+			
+		</table>
+	</div>           
 
 </body>
 
--- view/header.php
+++ view/header.php
@@ -14,18 +14,16 @@
     <!-- Custom CSS -->
     <link href="../resources/css/sb-admin-2.css" rel="stylesheet">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-    <style type="text/css">
-        .botao {
-            width: 100%;
-        }
-    </style>
+    <link href="../resources/css/header.css" rel="stylesheet">
 
+    <div class="navbar">
+        <a href = "Home.php"> Início  </a> 
+        <a href = "#Mensagens"> Mensagens  </a> 
+        <a href = "#Tarefas"> Tarefas  </a> 
+        <a href = "#Treinamentos"> Treinamentos  </a> 
+        <a href = "#Administração"> Administração  </a>   
+        <a href="#Sair">Sair</a>
+    </div>
+
+ 
 </head>
-
-<body>
